ec47f72aaa9461f87013794cadd4a84562f66805
akknapik <arkadiusz.knapik.2002@gmail.com>
2025-12-18 16:51:49 +0100

    feat: add charts implementation

--- src/app/components/generation-mix/generation-mix.component.ts
+++ src/app/components/generation-mix/generation-mix.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { EnergyService } from '../../services/energy.service';
 import { DailyMixDTO } from '../../models/daily-mix.dto';
 import { CommonModule } from '@angular/common';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartData, ChartOptions } from 'chart.js';
 import { catchError, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-generation-mix',
-  imports: [CommonModule],
+  imports: [CommonModule, BaseChartDirective],
   templateUrl: './generation-mix.component.html',
   styleUrl: './generation-mix.component.css'
 })
@@ -15,7 +17,22 @@ export class GenerationMixComponent {
   
   errorMessage: string | null = null;
   mix$: Observable<DailyMixDTO[]> = of([]);
+  chartCache = new Map<string, ChartData<'pie'>>();
 
+  chartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+    }
+  };
+
+  private colors: Record<string, string> = {
+    'wind': '#2ecc71', 'solar': '#f1c40f', 'nuclear': '#9b59b6',
+    'gas': '#e67e22', 'coal': '#34495e', 'biomass': '#27ae60',
+    'hydro': '#3498db', 'imports': '#95a5a6', 'other': '#7f8c8d'
+  };
 
   ngOnInit() {
     this.loadGenerationMix();
@@ -23,11 +40,31 @@ export class GenerationMixComponent {
 
   loadGenerationMix() {
     this.errorMessage = null;
+    
     this.mix$ = this.energyService.getGenerationMix().pipe(
       catchError(error => {
         this.errorMessage = error?.message ?? "An error occurred while fetching the generation mix data.";
         return of([] as DailyMixDTO[]);
       })
     );
+  }
+
+  getChartData(day: string, metrics: any[]): ChartData<'pie'> {
+    const cached = this.chartCache.get(day);
+
+    if(cached) {
+      return cached;
+    }
+
+    const data : ChartData<'pie'> = {
+      labels: metrics.map(m => m.fuelType),
+      datasets: [{
+        data: metrics.map(m => m.percentage),
+        backgroundColor: metrics.map(m => this.colors[m.fuelType] || '#bdc3c7'),
+      }]
+    } as ChartData<'pie'>;
+    this.chartCache.set(day, data);
+
+    return data;
   }
 }
